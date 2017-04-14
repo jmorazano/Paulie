d82391ca2c5e65f2cd8627b4b7aea5377601cb15
Juan Morazano <jmorazano@gmail.com>
2017-04-14 18:11:09 -0300

    bootstrap remove and css footer fix

--- header.php
+++ header.php
@@ -135,6 +135,3 @@ global $woo_options, $woocommerce;
 
 	</header><!-- /#header -->
 	<?php woo_content_before(); ?>
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
